1eade726688a97fa790055ffa946841535657dc4
Wiwin Mafiroh <wiwinmafiroh@gmail.com>
2021-07-30 20:44:35 +0700

    Menambahkan keterangan untuk setiap object, function, dan variabel

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,3 +1,4 @@
+// Untuk menyimpan data dan kondisi pada kalulator
 const calculator = {
   displayNumber: '0',
   operator: null,
@@ -5,10 +6,12 @@ const calculator = {
   waitingForSecondNumber: false
 }
 
+// Untuk meng-update angka pada layar kalkulator
 function updateDisplay() {
   document.querySelector("#displayNumber").innerText = calculator.displayNumber;
 }
 
+// Untuk menghapus data pada kalkulator
 function clearCalculator() {
   calculator.displayNumber = '0';
   calculator.operator = null;
@@ -16,12 +19,16 @@ function clearCalculator() {
   calculator.waitingForSecondNumber = false;
 }
 
+// Untuk memasukkan angka ke dalam nilai displayNumber kalkulator
 function inputDigit(digit) {
   calculator.displayNumber += digit;
 }
 
+// Menginisialisasikan nilai seluruh elemen button pada Variabel buttons
 const buttons = document.querySelectorAll(".button");
+// Melakukan looping
 for (let button of buttons) {
+  // Memberikan event click pada tiap elemen button
   button.addEventListener('click', function (event) {
     // mendapatkan objek elemen yang diklik
     const target = event.target;
