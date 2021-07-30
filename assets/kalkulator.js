6e0c4d7621e1f62f66b2dced134b30087165d66d
Wiwin Mafiroh <wiwinmafiroh@gmail.com>
2021-07-30 20:33:08 +0700

    Membuat variabel buttons, melakukan looping, dan memberikan event click

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -19,3 +19,14 @@ function clearCalculator() {
 function inputDigit(digit) {
   calculator.displayNumber += digit;
 }
+
+const buttons = document.querySelectorAll(".button");
+for (let button of buttons) {
+  button.addEventListener('click', function (event) {
+    // mendapatkan objek elemen yang diklik
+    const target = event.target;
+
+    inputDigit(target.innerText);
+    updateDisplay();
+  });
+}
