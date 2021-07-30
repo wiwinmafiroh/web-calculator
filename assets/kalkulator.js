d584d0032a693121ef54ab9f67ad553268d2257c
Wiwin Mafiroh <wiwinmafiroh@gmail.com>
2021-07-30 23:21:58 +0700

    Membuat fungsi performCalculation untuk melakukan kalkulasi

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -92,3 +92,20 @@ function handleOperator(operator) {
     alert('Operator sudah ditetapkan');
   }
 }
+
+// Untuk melakukan kalkulasi
+function performCalculation() {
+  if (calculator.firstNumber == null || calculator.operator == null) {
+    alert("Anda belum menetapkan operator");
+    return;
+  }
+
+  let result = 0;
+  if (calculator.operator === "+") {
+    result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+  } else {
+    result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+  }
+
+  calculator.displayNumber = result;
+}
